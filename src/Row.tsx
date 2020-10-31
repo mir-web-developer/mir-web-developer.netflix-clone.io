18dfd338e117bb5cf1fe59b3d4f6add4009f3c88
Mirjakhon Djalilov <djalilovmir@gmail.com>
2020-10-31 16:26:43 +0300

    last commit trailer is not working

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,17 +1,18 @@
 import Axios from "axios";
 import { request } from "https";
 import React, { useEffect, useState } from "react";
-import { reduceEachTrailingCommentRange } from "typescript";
-import axios from "./axios";
+import YouTube from "react-youtube";
+import { reduceEachTrailingCommentRange, transpile } from "typescript";
+import axios from "./axios"; //@ts-ignore
+//@ts-ignore
+import movieTrailer from "movie-trailer";
 import "./Row.css";
-
-
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = ({ isLargeRow, title, fetchUrl }:any) => {
+const Row = ({ isLargeRow, title, fetchUrl }: any) => {
   const [movies, setMovies] = useState([]);
-
+  const [trailerUrl, setTrailerUrl] = useState("");
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -22,23 +23,51 @@ const Row = ({ isLargeRow, title, fetchUrl }:any) => {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie: any) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url: any) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+// @ts-ignore
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error: any) => console.log(error));
+    }
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
-
       <div className="row__posters">
         {/*kdf*/}
         {movies.map((movie: any) => {
           return (
             <img
               key={movie.id}
+              onClick={() => {
+                handleClick(movie);
+              }}
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
               alt={movie.name}
             />
           );
         })}
       </div>
+      //@ts-ignore
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
